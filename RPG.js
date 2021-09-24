972969fd598ce673995be1af6caedcb654d2bdc7
Ben Bissett <benjaminbissett@gmail.com>
2021-09-24 13:00:01 -0400

    Added a prompt for the user to select their attack, a random attack for the enemy will be generated. When attacks are performed in the battle information is alerted to the user.

--- RPG.js
+++ RPG.js
@@ -4,9 +4,9 @@
 let player = {
     name: "Hercules",
     health: 100,
-    attackPower: 20,
+    attackPower: 10,
     defending: false,
-    attacks: ['Dodge', 'Uppercut', 'Round House Kick', 'Full Nelson']
+    attacks: ['Uppercut', 'Right Hook', 'Round House Kick', 'Full Nelson']
 }
 // Nemean Lion
 let lion = {
@@ -14,14 +14,14 @@ let lion = {
     descriptor: "The Vicious",
     health: 200,
     attackPower: 10,
-    attacks: ['Claw', 'Bite', 'Pounce', 'Evade']
+    attacks: ['Nibble', 'Claw', 'Bite', 'Pounce']
 }
 // Lernaean Hydra
 let hydra = {
     name: "Lernaean Hydra",
     descriptor: "The Impossible Nine-Headed",
     health: 300,
-    attackPower: 5,
+    attackPower: 20,
     attacks: ['Bite', 'Regenerate', 'Constrict']
 }
 // Cerberus
@@ -30,12 +30,23 @@ let dogo = {
     descriptor: "The Underworld Dogo"
 }
 
-
+// Function to run a battle between the player and the enemy
 function battle(player, enemy) {
     while (player.health > 0 && lion.health > 0) {
         function attack(attacker, defender) {
-            defender.health -= attacker.attackPower;
-            return defender;
+            if (attacker.name === player.name) {
+                let attackType = prompt("Choose your attack!\n1. Uppercut\n2. Right Hook\n3. Round House Kick\n4. Full Nelson");
+                defender.health -= attacker.attackPower * attackType;
+                alert(`${attacker.name} used ${attacker.attacks[attackType - 1]}`);
+                return defender;
+            }
+            else {
+                let attackType = Math.floor(Math.random() * 4);
+                defender.health -= attacker.attackPower * (attackType - 1);
+                alert(`${attacker.name} used ${attacker.attacks[attackType]}`);
+                return defender;
+            }
+            
         }
         if (player.defending === false) {
             enemy = attack(player, lion);
