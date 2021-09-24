b7f7dfc2af8777eca1ba6e4952c9b51b7c302acc
Ben Bissett <benjaminbissett@gmail.com>
2021-09-24 11:51:55 -0400

    Init

---
+++ RPG.js
@@ -0,0 +1,31 @@
+// JavaScript RPG
+
+// Hercules
+let player = {
+    name: "Hercules",
+    health: 100,
+    attackPower: 20,
+    attacks: ['Dodge', 'Uppercut', 'Round House Kick', 'Full Nelson']
+}
+// Nemean Lion
+let lion = {
+    name: "Nemean Lion",
+    descriptor: "The Vicious",
+    health: 200,
+    attackPower: 10,
+    attacks: ['Claw', 'Bite', 'Pounce', 'Evade']
+}
+// Lernaean Hydra
+let hydra = {
+    name: "Lernaean Hydra",
+    descriptor: "The Impossible Nine-Headed",
+    health: 300,
+    attackPower: 5,
+    attacks: ['Bite', 'Regenerate', 'Constrict']
+}
+// Cerberus
+let dogo = {
+    name: "Cerberus",
+    descriptor: "The Underworld Dogo"
+}
+
